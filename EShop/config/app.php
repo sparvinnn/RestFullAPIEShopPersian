51b93bd7d33c87dd0b7474979265a55f294d37ef
sparvinnn <sparvinnn@gmail.com>
2020-11-03 20:40:43 +0330

    modify database

--- EShop/config/app.php
+++ EShop/config/app.php
@@ -174,6 +174,11 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        /*
+         * Application Service Providers...
+         */
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+
 
     ],
 
