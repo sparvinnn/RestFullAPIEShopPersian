d418732c03230d0338e386ea86cd48e85478c0f1
mahankabir <mahan.kabir@gmail.com>
2020-11-29 09:22:13 +0330

    first commit

--- EShop/app/Models/Product.php
+++ EShop/app/Models/Product.php
@@ -5,12 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Product extends Model
 {
     use HasFactory;
-
-
-    public function products(){
-        return $this->hasMany('App/Models/Product');
-    }
 }
