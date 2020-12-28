7b11bcbbaf01c5c92c4b42f14b50610efbd876db
sparvinnn <sparvinnn@gmial.com>
2020-12-28 16:58:07 +0330

    edit category

--- EShop/app/Models/Category.php
+++ EShop/app/Models/Category.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = ['name', 'parent_id'];
+
+    public function subCategories(){
+        return $this->hasMany('App\Category', 'parent_id');
+    }
+
+//    public function parrentCategory(){
+//        return $this->belongsTo(Category::class);
+//    }
+
 }
--- EShop/database/migrations/2020_12_25_203429_create_categories_table.php
+++ EShop/database/migrations/2020_12_25_203429_create_categories_table.php
@@ -15,6 +15,9 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
