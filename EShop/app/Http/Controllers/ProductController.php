6a26244c6a1d5414464226a760e12148ab927856
sparvinnn <sparvinnn@gmial.com>
2021-01-03 23:43:17 +0330

    edit create product

--- EShop/app/Http/Controllers/ProductController.php
+++ EShop/app/Http/Controllers/ProductController.php
@@ -35,8 +35,9 @@ class ProductController extends Controller
                 ]);
             $properties = ProductProperty::where('product_id', $product->id)
                 ->get();
-            return response()->json(["status" => "success", "message" => "Success! registration completed", "product" => $product, "properties" => $properties]);
             DB::commit();
+            return response()->json(["status" => "success", "message" => "Success! registration completed", "product" => $product, "properties" => $properties]);
+
         }catch (\Exception $e){
             DB::rollBack();
             return response()->json(["status" => "failed", "message" => $e]);
@@ -45,5 +46,36 @@ class ProductController extends Controller
 
     }
 
+    public function update(Request $request, $id){
+//        try{
+            DB::beginTransaction();
 
+            $inputs = $request->product[0];
+            return $product   =   Product::find($id);
+            $product->title = $inputs->title;
+            $product->price = $inputs->price;
+            $product->description = $inputs->description;
+            $product->category_id = $inputs->category_id;
+            $product->branch_id = $inputs->branch_id;
+            $product->save();
+            return $product;
+            $inputs = $request->properties[0];
+            ProductProperty::where('product_id', $product->id)->delete();
+            foreach($inputs as $input){
+                ProductProperty::create([
+                    'product_id'=>$product->id,
+                    'property_id'=>$input[0]['property_id'],
+                    'value'=>$input[0]['value'],
+                ]);
+            }
+
+            $properties = ProductProperty::where('product_id', $product->id)
+                ->get();
+            return response()->json(["status" => "success", "message" => "Success! registration completed", "product" => $product, "properties" => $properties]);
+            DB::commit();
+//        }catch (\Exception $e){
+//            DB::rollBack();
+//            return response()->json(["status" => "failed", "message" => $e]);
+//        }
+    }
 }
